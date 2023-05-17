c4ac34611d568cbc4d7220d352e90a362394640e
Achyut Z <achyut.zalavadia@visilean.com>
2023-05-17 19:32:49 +0530

    Added toggle dark mode functionality in Navbar.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 // import logo from './logo.svg';
+import React, { useState } from "react";
 import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
+  //#212529 hexcode for dark colour of navbar 
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey'
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
     <>
       {/* <Navbar title="TextUtils" about="About TextUtils"/> */}
       {/* <Navbar/> */}
-      <Navbar title="TextUtils" />
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
         {/* <About heading="About us"></About> */}
         <TextForm heading="Enter the text to analyze below" />
