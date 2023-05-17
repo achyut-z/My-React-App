6734a13667c6688dd04c70f0ddefcd75120b633a
Achyut Z <achyut.zalavadia@visilean.com>
2023-05-17 15:06:49 +0530

    Added files for navbar and text area

---
+++ src/components/TextForm.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleLoClick = () => {
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
+
+    const handleClearClick = () => {
+        let newText = "";
+        setText(newText)
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+    const [text, setText] = useState("");
+    // setText("gvdfbfghfgf")
+    return (
+        <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" onChange={handleOnChange} value= {text} rows="8"></textarea>
+            </div>
+            <button className="btn btn-outline-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-outline-primary mx-3" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-outline-primary mx-3" onClick={handleClearClick}>Clear text</button>
+        </div>
+        <div className="container my-3">
+            <h1>Your text summary</h1>
+            <p>{text.split(" ").length} words, {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
+    )
+}
